322a284cdc17a8fe6483313775f8d03820fd5333
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-27 02:49:06 +0530

    assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import ellipsisIcon from "./icon-ellipsis.svg";
+import exerciseIcon from "./icon-exercise.svg";
+import playIcon from "./icon-play.svg";
+import selfCareIcon from "./icon-self-care.svg";
+import socialIcon from "./icon-social.svg";
+import studyIcon from "./icon-study.svg";
+import workIcon from "./icon-work.svg";
+import jeremyImage from "./image-jeremy.png";
+
+export {
+  ellipsisIcon,
+  exerciseIcon,
+  playIcon,
+  selfCareIcon,
+  socialIcon,
+  studyIcon,
+  workIcon,
+  jeremyImage,
+};
